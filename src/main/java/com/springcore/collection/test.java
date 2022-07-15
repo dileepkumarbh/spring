6d69d2a373d16fd4401ab66fc31cd86eb4e19a9d
dileepkumar001 <hosteldileep01@gmail.com>
2022-07-15 23:49:46 +0530

    ref

---
+++ src/main/java/com/springcore/collection/test.java
@@ -0,0 +1,19 @@
+package com.springcore.collection;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ApplicationContext context=new ClassPathXmlApplicationContext("com/springcore/collection/cconfig.xml");
+		Emp emp1=(Emp) context.getBean("emp1");
+		System.out.println(emp1.getName());
+		System.out.println(emp1.getPhones());
+		System.out.println(emp1.getAddress());
+		System.out.println(emp1.getCourses());
+
+	}
+
+}
